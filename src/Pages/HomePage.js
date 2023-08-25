8ee228fe8f38298f352e4666c06d76b9aca63190
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2023-08-25 20:18:03 +0530

    meta update 3

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -19,7 +19,7 @@ function HomePage(props) {
             <div className="typography">
                 <h1>Hi, I'm <span>Vaibhav Bansal</span></h1>
                 <p>
-                    I am an Indian based Software developer and Data Science enthusiast. Currently working as Front-end Developer @ Dashclicks India Ltd.
+                    I am an India based Software developer and Data Science enthusiast. Currently working as Front-end Developer @ Dashclicks India Ltd.
                 </p>
                 <div className="icons">
                     <a href="https://www.facebook.com/vaibhavcoo" className="icon i-facebook">
